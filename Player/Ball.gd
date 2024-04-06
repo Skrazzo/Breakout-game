af91043a60d8f717954e9b0f8534b833facfda55
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-04-06 22:17:17 +0300

    Made pretty walls

--- Player/Ball.gd
+++ Player/Ball.gd
@@ -4,19 +4,22 @@ var rng = RandomNumberGenerator.new()
 var speed = 500
 
 # Generate random start direction for the ball
-var direction = Vector2(rng.randf_range(-1, 1), rng.randf_range(-0.5, -1))
+var direction = Vector2(rng.randf_range(-1, 1), rng.randf_range(-0.6, -1.5))
 var isActive = false
+var gameOver = true
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	position.x = get_viewport_rect().size.x / 2
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed('fire'):
+	if Input.is_action_pressed('fire') and gameOver:
 		isActive = true
+		gameOver = false
 	
 	# if isActive isn't false, then ball does not move, so that means that ball calculations do not happen
 	if !isActive:
@@ -26,6 +29,16 @@ func _process(delta):
 	var collision = move_and_collide(velocity)
 	
 	if collision != null:
+		print(collision.get_collider_id())
 		direction = direction.bounce(collision.get_normal())
 	
+	# Check if the ball is on the screen
+	
 	pass
+
+
+# Ball is not visible on the screen, so we need to restart our main scene
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	gameOver = true
+	get_tree().change_scene_to_file("res://node_2d.tscn")
+	pass # Replace with function body.
